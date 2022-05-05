953fd869c904643708c46f4e2ebc8d8d0c5a5a88
lkhan69420 <b2022lskhan@pshs.edu.ph>
2022-05-06 04:58:56 +0800

    initial big commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// window.addEventListener("scroll", function() {
+//     var elementTarget = document.getElementById("test");
+//     if (window.scrollY > (elementTarget.offsetTop + elementTarget.offsetHeight)) {
+//         alert("You've scrolled past the second div");
+//     }
+// });
+
+document.getElementById("scrollable").addEventListener("scroll", function() {
+    alert("fuck");
+});
